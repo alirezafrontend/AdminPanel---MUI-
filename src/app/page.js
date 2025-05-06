ee8a500b978098317e2c4e5cb50b7b216ca2326b
Alireza hasanpour <alirezahasanpourweb19921371@gmail.com>
2025-05-06 14:05:28 +0330

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
 "use client";
-import Login from "@/components/login/login";
+
+import Loading from "@/components/Loading";
+import Login from "@/components/login/Login";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loading />;
   return <Login />;
 }
